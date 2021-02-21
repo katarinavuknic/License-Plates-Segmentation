b67bea2c677b6d0e99912232a3111ec8470c8f61
NediljkoIvisic <nediljko.ivisic99@gmail.com>
2021-02-21 15:25:33 +0100

    Changing Dmin value

--- detectLicencePlate.py
+++ detectLicencePlate.py
@@ -37,7 +37,7 @@ def preprocess_image(image_path,resize=False):
         img = cv2.resize(img, (224,224))
     return img
 
-def get_plate(image_path, Dmax=608, Dmin = 256):
+def get_plate(image_path, Dmax=608, Dmin = 400):
     vehicle = preprocess_image(image_path)
     ratio = float(max(vehicle.shape[:2])) / min(vehicle.shape[:2])
     side = int(ratio * Dmin)
@@ -45,7 +45,7 @@ def get_plate(image_path, Dmax=608, Dmin = 256):
     _ , LpImg, _, cor = detect_lp(wpod_net, vehicle, bound_dim, lp_threshold=0.5)
     return vehicle, LpImg, cor
 
-test_image_path = "CarExamples/5.jpg"
+test_image_path = "CarExamples/1.jpg"
 vehicle, LpImg, cor = get_plate(test_image_path)
 
 fig = plt.figure(figsize=(12,6))
@@ -57,5 +57,122 @@ grid = gridspec.GridSpec(ncols=2,nrows=1,figure=fig)
 fig.add_subplot(grid[1])
 plt.axis(False)
 plt.imshow(LpImg[0])
+
+fig = plt.figure(figsize=(12,6))
+plt.axis(False)
+plt.imshow(LpImg[0])
+
+# Scales, calculates absolute values, and converts the result to 8-bit.
+plate_image = cv2.convertScaleAbs(LpImg[0], alpha=(255.0))
+
+# convert to grayscale and blur the image
+gray = cv2.cvtColor(plate_image, cv2.COLOR_BGR2GRAY)
+blur = cv2.GaussianBlur(gray,(7,7),0)
+    
+# Applied inversed thresh_binary 
+ret,binary = cv2.threshold(blur, 180, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    
+kernel3 = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+thre_mor = cv2.morphologyEx(binary, cv2.MORPH_DILATE, kernel3)
+
+# visualize results    
+fig = plt.figure(figsize=(12,7))
+plt.rcParams.update({"font.size":18})
+grid = gridspec.GridSpec(ncols=2,nrows=3,figure = fig)
+plot_image = [plate_image, gray, blur, binary,thre_mor]
+plot_name = ["plate_image","gray","blur","binary","dilation"]
+
+for i in range(len(plot_image)):
+    fig.add_subplot(grid[i])
+    plt.axis(False)
+    plt.title(plot_name[i])
+    if i ==0:
+        plt.imshow(plot_image[i])
+    else:
+        plt.imshow(plot_image[i],cmap="gray")
+
+# plt.savefig("threshding.png", dpi=300)
+
+# Create sort_contours() function to grab the contour of each digit from left to right
+def sort_contours(cnts,reverse = False):
+    i = 0
+    boundingBoxes = [cv2.boundingRect(c) for c in cnts]
+    (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
+                                        key=lambda b: b[1][i], reverse=reverse))
+    return cnts
+
+cont, _  = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# creat a copy version "test_roi" of plat_image to draw bounding box
+test_roi = plate_image.copy()
+
+# Initialize a list which will be used to append charater image
+crop_characters = []
+
+# define standard width and height of character
+digit_w, digit_h = 30, 60
+
+for c in sort_contours(cont):
+    (x, y, w, h) = cv2.boundingRect(c)
+    ratio = h/w
+    if 1<=ratio<=10: # Only select contour with defined ratio
+        if 0.8>=h/plate_image.shape[0]>=0.5: # Select contour which has the height larger than 50% of the plate
+            # Draw bounding box arroung digit number
+            cv2.rectangle(test_roi, (x, y), (x + w, y + h), (0, 255,0), 2)
+
+            # Sperate number and gibe prediction
+            curr_num = thre_mor[y:y+h,x:x+w]
+            curr_num = cv2.resize(curr_num, dsize=(digit_w, digit_h))
+            _, curr_num = cv2.threshold(curr_num, 220, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+            crop_characters.append(curr_num)
+
+print("Detect {} letters...".format(len(crop_characters)))
+fig = plt.figure(figsize=(10,6))
+plt.axis(False)
+plt.imshow(test_roi)
+#plt.savefig('grab_digit_contour.png',dpi=300)
+
+plt.style.use('classic')
+fig = plt.figure(figsize=(10,6))
+grid = gridspec.GridSpec(ncols=len(crop_characters),nrows=1,figure=fig)
+
+for i in range(len(crop_characters)):
+    fig.add_subplot(grid[i])
+    plt.axis(False)
+    plt.imshow(crop_characters[i],cmap="gray")
+#plt.savefig("segmented_leter.png",dpi=300)
+
+json_file = open('MobileNets_character_recognition.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+model = model_from_json(loaded_model_json)
+model.load_weights("License_character_recognition_weight.h5")
+print("[INFO] Model loaded successfully...")
+
+labels = LabelEncoder()
+labels.classes_ = np.load('license_character_classes.npy')
+print("[INFO] Labels loaded successfully...")
+
+def predict_from_model(image,model,labels):
+    image = cv2.resize(image,(80,80))
+    image = np.stack((image,)*3, axis=-1)
+    prediction = labels.inverse_transform([np.argmax(model.predict(image[np.newaxis,:]))])
+    return prediction
+
+fig = plt.figure(figsize=(15,3))
+cols = len(crop_characters)
+grid = gridspec.GridSpec(ncols=cols,nrows=1,figure=fig)
+
+final_string = ''
+for i,character in enumerate(crop_characters):
+    fig.add_subplot(grid[i])
+    title = np.array2string(predict_from_model(character,model,labels))
+    plt.title('{}'.format(title.strip("'[]"),fontsize=20))
+    final_string+=title.strip("'[]")
+    plt.axis(False)
+    plt.imshow(character,cmap='gray')
+
+print("\n\n\n",final_string)
+#plt.savefig('final_result.png', dpi=300)
 
 plt.show()
